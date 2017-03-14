4d24d79ad9a39caac39d069a70586aa53de203bc
Soniafish <linke235@gmail.com>
2017-03-14 16:20:53 +0800

    change code from js to jq

--- health/health/js/page1.js
+++ health/health/js/page1.js
@@ -1,43 +1,15 @@
-var tv;
-var management1;
-var hot1;
-var news1;
-var topic1;
-var content;
-var management2;
-var hot2;
-var news2;
-var topic2;
 var video;
 var WorkOut7m;
 var FIGUREROBICS;
 var yoga;
-var bmiHeight;
-var bmiWeight;
-var bmi;
 var stepData;
-
-window.onload=function () {
-	tv=document.getElementById("tv");
-	management1=document.getElementById("management1");
-	hot1=document.getElementById("hot1");
-	news1=document.getElementById("news1");
-	topic1=document.getElementById("topic1");
-	content=document.getElementById("content");
-	management2=document.getElementById("management2");
-	hot2=document.getElementById("hot2");
-	news2=document.getElementById("news2");
-	topic2=document.getElementById("topic2");
-
+$(document).ready(function (){
 	// TV
 	WorkOut7m=document.getElementById("WorkOut7m");
 	WorkOut7m.play();
 	video=WorkOut7m;
 	FIGUREROBICS=document.getElementById("FIGUREROBICS");
 	yoga=document.getElementById("yoga");
-
-	//BMI
-	bmi=document.getElementById("bmi");
 
 	//google chart & localstorage
 	stepData=window.localStorage.getItem("stepData");
@@ -50,114 +22,112 @@ window.onload=function () {
 		console.log(stepData);
 	}else{
 		stepData=JSON.parse(stepData);
-	}	
+	}
+});
 
-};
 	function changeToTv() {
-		tv.style.color="#0000FF";
-		management1.style.color="#B9B9FF";
-		hot1.style.color="#B9B9FF";
-		news1.style.color="#B9B9FF";
-		topic1.style.color="#B9B9FF";
+		$("#tv").css("color", "#0000FF");
+		$("#management1").css("color", "#B9B9FF");
+		$("#hot1").css("color", "#B9B9FF");
+		$("#news1").css("color", "#B9B9FF");
+		$("#topic1").css("color", "#B9B9FF");
 
-		content.style.display="block";
+		$("#content").css("display", "block");
 		showWorkOut7m();
-		management2.style.display="none";
-		hot2.style.display="none";
-		news2.style.display="none";
-		topic2.style.display="none";
+		$("#management2").css("display", "none");
+		$("#hot2").css("display", "none");
+		$("#news2").css("display", "none");
+		$("#topic2").css("display", "none");
 	}
 	function changeTomanagement(){
-		tv.style.color="#B9B9FF";
-		management1.style.color="#0000FF";
-		hot1.style.color="#B9B9FF";
-		news1.style.color="#B9B9FF";
-		topic1.style.color="#B9B9FF";
+		$("#tv").css("color", "#B9B9FF");
+		$("#management1").css("color", "#0000FF");
+		$("#hot1").css("color", "#B9B9FF");
+		$("#news1").css("color", "#B9B9FF");
+		$("#topic1").css("color", "#B9B9FF");
 
-		content.style.display="none";
+		$("#content").css("display", "none");
 		WorkOut7m.pause();
 		FIGUREROBICS.pause();
 		yoga.pause();
-		management2.style.display="block";
-		hot2.style.display="none";
-		news2.style.display="none";
-		topic2.style.display="none";
+		$("#management2").css("display", "block");
+		$("#hot2").css("display", "none");
+		$("#news2").css("display", "none");
+		$("#topic2").css("display", "none");
 	}	
 	function changeToHot() {
-		tv.style.color="#B9B9FF";
-		management1.style.color="#B9B9FF";
-		hot1.style.color="#0000FF";
-		news1.style.color="#B9B9FF";
-		topic1.style.color="#B9B9FF";
+		$("#tv").css("color", "#B9B9FF");
+		$("#management1").css("color", "#B9B9FF");
+		$("#hot1").css("color", "#0000FF");
+		$("#news1").css("color", "#B9B9FF");
+		$("#topic1").css("color", "#B9B9FF");
 
-		content.style.display="none";
+		$("#content").css("display", "none");
 		WorkOut7m.pause();
 		FIGUREROBICS.pause();
 		yoga.pause();
-		management2.style.display="none";
-		hot2.style.display="block";
-		news2.style.display="none";
-		topic2.style.display="none";
+		$("#management2").css("display", "none");
+		$("#hot2").css("display", "block");
+		$("#news2").css("display", "none");
+		$("#topic2").css("display", "none");
 	}
 
 	function changeToNews() {
-		tv.style.color="#B9B9FF";
-		management1.style.color="#B9B9FF";
-		hot1.style.color="#B9B9FF";
-		news1.style.color="#0000FF";
-		topic1.style.color="#B9B9FF";
+		$("#tv").css("color", "#B9B9FF");
+		$("#management1").css("color", "#B9B9FF");
+		$("#hot1").css("color", "#B9B9FF");
+		$("#news1").css("color", "#0000FF");
+		$("#topic1").css("color", "#B9B9FF");
 
-		content.style.display="none";
+		$("#content").css("display", "none");
 		WorkOut7m.pause();
 		FIGUREROBICS.pause();
 		yoga.pause();
-		management2.style.display="none";
-		hot2.style.display="none";
-		news2.style.display="block";
-		topic2.style.display="none";
+		$("#management2").css("display", "none");
+		$("#hot2").css("display", "none");
+		$("#news2").css("display", "block");
+		$("#topic2").css("display", "none");
 	}
 
 	function changeToTopic() {
-		tv.style.color="#B9B9FF";
-		management1.style.color="#B9B9FF";
-		hot1.style.color="#B9B9FF";			
-		news1.style.color="#B9B9FF";
-		topic1.style.color="#0000FF";
+		$("#tv").css("color", "#B9B9FF");
+		$("#management1").css("color", "#B9B9FF");
+		$("#hot1").css("color", "#B9B9FF");
+		$("#news1").css("color", "#B9B9FF");
+		$("#topic1").css("color", "#0000FF");
 
-		content.style.display="none";
+		$("#content").css("display", "none");
 		WorkOut7m.pause();
 		FIGUREROBICS.pause();
 		yoga.pause();
-		management2.style.display="none";
-		hot2.style.display="none";
-		news2.style.display="none";
-		topic2.style.display="block";
+		$("#management2").css("display", "none");
+		$("#hot2").css("display", "none");
+		$("#news2").css("display", "none");
+		$("#topic2").css("display", "block");
 	}
 
 	function changeToBack() {
-		tv.style.color="#B9B9FF";
-		management1.style.color="#B9B9FF";
-		hot1.style.color="#B9B9FF";			
-		news1.style.color="#B9B9FF";
-		topic1.style.color="#B9B9FF";
-
-		content.style.display="block";
-		showWorkOut7m();
-		management2.style.display="none";
-		hot2.style.display="none";
-		news2.style.display="none";
-		topic2.style.display="none";
+		changeToTv();
+		$("#tv").css("color", "#B9B9FF");
 	}
 
 	// TV
 		function showWorkOut7m(){
-		WorkOut7m.style.display="block";
+		$("#WorkOut7m").css("display", "block");
 		WorkOut7m.play();
-		FIGUREROBICS.style.display="none";
+		$("#FIGUREROBICS").css("display", "none");
 		FIGUREROBICS.pause();
-		yoga.style.display="none";
+		$("#yoga").css("display", "none");
 		yoga.pause();
 		video=WorkOut7m;
+
+		// FWorkOut7m.style.display="block";
+		// WorkOut7m.play();
+		// FIGUREROBICS.style.display="none";
+		// FIGUREROBICS.pause();
+		// yoga.style.display="none";
+		// yoga.pause();
+		// video=WorkOut7m;
 	}
 
 	function showFIGUREROBICS(){
@@ -181,17 +151,20 @@ window.onload=function () {
 	}
 	//BMI
 	function getBmi(){
-		bmiHeight=document.getElementById("bmiHeight").value;
-		bmiWeight=document.getElementById("bmiWeight").value;
+		var bmiHeight=$("#bmiHeight").val();
+		var bmiWeight=$("#bmiWeight").val();
+
+		// bmiHeight=document.getElementById("bmiHeight").value;
+		// bmiWeight=document.getElementById("bmiWeight").value;
 		var h = Number(bmiHeight);
     	var w = Number(bmiWeight);
     	var yourBmi = w / (h * h) * 10000;
     	if (yourBmi>24) {
-    		bmi.innerHTML="BMI:"+Math.round(yourBmi*100)/100+",過重";
+    		$("#bmi").html("BMI:"+Math.round(yourBmi*100)/100+",過重");
     	}else if(yourBmi<18.5){
-    		bmi.innerHTML="BMI:"+Math.round(yourBmi*100)/100+",過輕";
+    		$("#bmi").html("BMI:"+Math.round(yourBmi*100)/100+",過輕");
     	}else{
-    		bmi.innerHTML="BMI:"+Math.round(yourBmi*100)/100+",健康";
+    		$("#bmi").html("BMI:"+Math.round(yourBmi*100)/100+",健康");
     	}
 	}
  //google chart
